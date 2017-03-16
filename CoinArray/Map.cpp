dcf32e66cb00021b7737b3ebba26b0829c66669b
polblanescebrian <polblanescebrian@S1-45>
2017-03-16 09:51:37 +0100

    map

--- CoinArray/Map.cpp
+++ CoinArray/Map.cpp
@@ -1,12 +1,54 @@
-#include "Map.h"
+#include "map.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
-
-
-Map::Map()
+Map::Map(int difficulty) :
+	difficulty(difficulty),
+	rows(5 * difficulty + rand() % 11),
+	columns(rows)
 {
+	md = new char*[rows];
+	for (int i = 0; i < rows; i++)
+		md[i] = new char[columns];
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			md[i][j] = '.';
+		}
+	}
 }
 
-
-Map::~Map()
+Map::~Map(void)
 {
+	for (int i = 0; i < rows; i++)
+		delete md[i];
+	delete[] md;
+}
+
+//Cell modifier method
+void Map::cellUpdate(int i, int j, char c)
+{
+	md[i][j] = c;
+}
+
+int Map::size()
+{
+	return rows;
+}
+
+char Map::cell(int i, int j)
+{
+	return md[i][j];
+}
+
+std::ostream & operator<<(std::ostream & out, Map x)
+{
+	// TODO: insert return statement here
+	for (int i = 0; i < x.size(); i++) {
+		for (int j = 0; j < x.size(); j++) {
+			out << x.cell(i, j) << " ";
+		}
+		out << std::endl;
+	}
+	return out;
 }
--- CoinArray/Map.h
+++ CoinArray/Map.h
@@ -1,8 +1,30 @@
 #pragma once
+#include <ostream>
+
 class Map
 {
 public:
-	Map();
-	~Map();
-};
+	//constructors
+	Map(int difficulty);
 
+	//destructor
+	~Map(void);
+
+	//Cell modifier method
+	void cellUpdate(int i, int j, char c);
+
+	//output stream operator overload
+	friend std::ostream& operator<< (std::ostream &out, Map x);
+
+	//Atribute retrieving methods
+	int size();
+	char cell(int i, int j);
+
+private:
+
+	char **md;
+	int difficulty;
+	int rows;
+	int columns;
+
+};
