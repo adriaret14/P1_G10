6b049d068d6281938137cd433e3c8995844ea0ae
polblanescebrian <polblanescebrian@S1-45>
2017-03-21 12:25:54 +0100

    coinManager.h fix

--- CoinArray/CoinManager.h
+++ CoinArray/CoinManager.h
@@ -6,7 +6,7 @@ public:
 	CoinManager(Map map);							//Constructor
 	~CoinManager();									//Destructor
 	void updateCoins(Map map);						//coin update method
-	void CoinManager::createCoins(Map map);			//coin creation method
+	void createCoins(Map map);			//coin creation method
 private:
 	int coins;
 };
