3fc95a7290c01f9da5759b292e7e56d4f4e98263
Pol Blanes Cebrian <polblanescebrian@enti.cat>
2017-03-27 15:20:22 +0200

    timer y game over screen

--- CoinArray/Main.cpp
+++ CoinArray/Main.cpp
@@ -9,9 +9,11 @@
 void main() {
 	srand(static_cast<unsigned int>(time(nullptr)));
 	int dificultad;
+	double timeStart, timeNow;
+	int mins, secs;
 
 	//Pedimos la dificultad del juego
-	do{
+	do {
 		system("cls");
 		std::cout << "Escoge una dificultad de juego:\n\t-FACIL(1)\n\t-MEDIO(2)\n\t-DIFICIL(3)\n";
 		std::cin >> dificultad;
@@ -34,19 +36,33 @@ void main() {
 	//std::cout << m;
 	//p.updatePlayer(Input::Key::W);
 	//system("cls");
-	std::cout << m << std::endl << "BB HEADSHOTS: " << p.getScore() << "/" << nMonedas;
+	timeStart = clock();
+	std::cout << m << std::endl << "Monedas: " << p.getScore() << "/" << nMonedas;
 	//std::cout << m;
 	Input::Key input;
 	do {
+		timeNow = clock();
+		mins = static_cast<int>((timeNow - timeStart) / static_cast<double>(CLOCKS_PER_SEC) / 60);
+		secs = static_cast<int>((timeNow - timeStart) / static_cast<double>(CLOCKS_PER_SEC)) % 60;
 		input = Input::getKey();
 		if (input != Input::Key::NONE) {
 			p.updatePlayer(input);
 			system("cls");
-			std::cout << m << std::endl << "BB HEADSHOTS: " << p.getScore() << "/" << nMonedas ;
+			std::cout << m << std::endl << "Monedas: " << p.getScore() << "/" << nMonedas;
 		}
 	} while ((input != Input::Key::ESC) && (p.getScore() < nMonedas));
-	
+
 	//m.~Map();
 	system("cls");
-	std::cout << "Fin de la partida";
+	std::cout << "\t" << "Fin de la partida" << std::endl;
+	if (!(p.getScore() < nMonedas))
+	{
+		std::cout << std::endl;
+		std::cout << "Monedas conseguidas: \t" << p.getScore() << std::endl;
+		std::cout << "Tiempo invertido: \t" << mins << ":" << secs << std::endl;
+	}
+	do
+	{
+		input = Input::getKey();
+	} while (input != Input::Key::ESC);
 }
